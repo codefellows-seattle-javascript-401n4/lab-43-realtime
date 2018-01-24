8a423402c1bed78c80e953f5144a7874c2d3ed30
David Boucher <david.boucher@ookla.com>
2018-01-23 21:18:35 -0800

    rejig starter code

--- starter-code/client/src/components/chat/actions.js
+++
@@ -1,6 +0,0 @@
-// all payloads: 
-// {username: string, meta: bool, content: string}
-
-// Actions will be dispatched from subscribers that hear messages coming directly to us from the server!
-
-// TODO: Create actions: USER_CONNECTED, USER_DISCONNECTED, MESSAGE
