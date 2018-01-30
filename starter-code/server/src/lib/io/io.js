109ff02a9ffcba682141b729d3a9940d8ace29b8
sgordon834 <sgordon834@gmail.com>
2018-01-29 22:48:59 -0800

    works lets chat

--- starter-code/server/src/lib/io/io.js
+++ starter-code/server/src/lib/io/io.js
@@ -6,26 +6,27 @@ import messageSubscriber from "./subscribers/message";
 const subscribers = Object.assign({}, authSubscriber, messageSubscriber)
 
 export default (http) => {
+    
     return io(http)
         // This will run when the client first connects to our socket
         // It registers all of the listeners for THEM
         .on('connection', (socket) => {
             
-            Object.keys(subscribers)
-                .map(type => {
+            Object.keys(subscribers)  // [MESSAGE,LOGIN,LOGOUT]
+                .map( type => {
                     let handler = subscribers[type];
-                    return{type, handler};
+                    return {type,handler};
                 })
                 .forEach(subscriber => {
                     socket.on(subscriber.type, (payload) => {
+                        console.log('_SUBSCRIBE_EVENT_', subscriber.type, payload);
                         try {
-                        subscriber.handler(socket)(payload);
+                            subscriber.handler(socket)(payload);
                         }
                         catch(e) {
-                            console.error('_SUBSCRIBE_ERROR_', e.message)
+                            console.error('_SUBSCRIBE_ERROR_', e.message);
                         }
                     })
-                    // TODO: do a "socket.on" for the subscriber.type that takes payload and tries to run its handler
                 })
         })
         .on('error', (error) => {
